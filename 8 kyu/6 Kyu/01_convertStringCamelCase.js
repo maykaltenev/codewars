73484d12caf7803e22288aadef6e64d9dd129d86
maykaltenev <michaeltenev@gmail.com>
2022-04-12 18:29:42 +0200

    01

---
+++ 8 kyu/6 Kyu/01_convertStringCamelCase.js
@@ -0,0 +1,16 @@
+function toCamelCase(str) {
+    let result = [];
+    if (str.contains('-')) {
+        result = str.split('-');
+    } else if (str.contains('_')) {
+        result = str.split('_');
+    }
+    let toUpperCase = [];
+    for (let i = 0; i < result.length; i++) {
+        rel;
+        let spliced = result
+    }
+}
+
+
+console.log(toCamelCase('the_stealth_warrior'))

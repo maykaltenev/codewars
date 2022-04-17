e671bf994de06127fb6d4ea4b85a3dfee4d35d57
maykaltenev <michaeltenev@gmail.com>
2022-04-17 22:46:58 +0200

    28

---
+++ 8 kyu/28_quaterOfYear.js
@@ -0,0 +1,17 @@
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quaconst quarterOf = (month) => {
+// Your code here
+
+const quarterOf = (month) => {
+    if (month <= 3) {
+        return 1;
+    } else if (month <= 6) {
+        return 2;
+    } else if (month <= 9) {
+        return 3;
+    } else if (month <= 12) {
+        return 4;
+    } else {
+        return 0;
+    }
+}
